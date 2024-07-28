bf63f1e9b176608e1f877be4e10325eb635ee5e0
QuentinHsu <xuquentinyang@gmail.com>
2024-07-28 20:58:28 +0800

    chore: add @radix-ui/react-select and Skeleton component
    The commit adds the `@radix-ui/react-select` dependency to the `package.json` file. Additionally, a new `Skeleton` component is added to the `components/ui` directory. The `Skeleton` component is a simple UI component that renders a pulsating animation.

--- components/config/FormContent.tsx
+++ components/config/FormContent.tsx
@@ -14,6 +14,8 @@ import {
 } from "components/ui/form";
 
 import { Textarea } from 'components/ui/textarea';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue, SelectGroup} from 'components/ui/select';
+import { Skeleton } from 'components/ui/skeleton';
 
 // 定义表单的验证模式
 const formSchema = z.object({
@@ -22,6 +24,12 @@ const formSchema = z.object({
   }),
   secondLanguage: z.string().min(1, {
     message: "第二语言必须至少包含 1 个字符。",
+  }),
+  fontFamily: z.string().min(1, {
+    message: "字体必须至少包含 1 个字符。",
+  }),
+  layout: z.string().min(1, {
+    message: "布局必须至少包含 1 个字符。",
   }),
 });
 
@@ -32,6 +40,8 @@ const FormContent: React.FC = () => {
     defaultValues: {
       firstLanguage: "",
       secondLanguage: "",
+      fontFamily: "serif",
+      layout: ""
     },
   });
 
@@ -71,6 +81,79 @@ const FormContent: React.FC = () => {
               </FormControl>
               <FormDescription>
                 这里是第二语言内容。
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="fontFamily"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>字体</FormLabel>
+              <FormControl>
+                <Select value={field.value} name={field.name} onValueChange={field.onChange}>
+                  <SelectTrigger>
+                    <SelectValue placeholder="选择字体" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="serif">Serif</SelectItem>
+                    <SelectItem value="sans-serif">Sans-serif</SelectItem>
+                    <SelectItem value="monospace">Monospace</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormDescription>
+                这里是字体内容。
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="layout"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>布局</FormLabel>
+              <FormControl>
+                <Select value={field.value} name={field.name} onValueChange={field.onChange}>
+                  <SelectTrigger>
+                    <SelectValue placeholder="选择布局" />
+                  </SelectTrigger>
+                  <SelectContent className='flex'>
+                    <SelectGroup>
+                      <SelectItem value="one" className='flex'>
+                        One
+                      </SelectItem>
+                      <div className="p-2">
+                        <Skeleton className="h-6 m-1 w-[50%]" >
+                          <span className='text-sm text-slate-400 p-2'>第一语言</span>
+                        </Skeleton>
+                        <Skeleton className="h-6 m-1 w-[50%]">
+                          <span className='text-sm text-slate-400 p-2'>第二语言</span>
+                        </Skeleton>
+                      </div>
+                    </SelectGroup>
+                    <SelectGroup>
+                      <SelectItem value="two">
+                        Two
+                      </SelectItem>
+                      <div className="p-2">
+                        <Skeleton className="h-6 m-1 w-[50%]">
+                          <span className='text-sm text-slate-400 p-2'>第二语言</span>
+                        </Skeleton>
+                        <Skeleton className="h-6 m-1 w-[50%]" >
+                          <span className='text-sm text-slate-400 p-2'>第一语言</span>
+                        </Skeleton>
+                      </div>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormDescription>
+                这里是布局选项。
               </FormDescription>
               <FormMessage />
             </FormItem>
